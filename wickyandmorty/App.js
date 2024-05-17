5c444f35961c6fe15eb81f0df7f1cdb71fc3d4bb
martifontanet <marti.fontanet@estudiantat.upc.edu>
2024-05-17 11:33:06 +0200

    Auth v5

--- wickyandmorty/App.js
+++ wickyandmorty/App.js
@@ -6,62 +6,30 @@ import TabIcon from "./components/Basic/TabIcons";
 import HomeTab from "./tabs/HomeTab";
 import WikiTab from "./tabs/WikiTab";
 import PublishTab from "./tabs/PublishTab";
-import { AuthProvider } from "./utils/userContext";
+import { AuthProvider, useUserInfo } from "./utils/userContext";
+import UserAuth from "./screens/UserAuth";
+import Navigation from "./utils/Navigation";
+import { useEffect, useState } from "react";
 
 const Tab = createBottomTabNavigator();
-
 
 export default function App() {
   return (
     <AuthProvider>
-        <NavigationContainer>
-          <Tab.Navigator 
-            screenOptions={{
-              tabBarStyle: styles.tabBar,
-              tabBarLabelStyle: { fontSize: 14, },
-              tabBarInactiveTintColor: "white",
-              tabBarActiveTintColor: "#97CE4C",
-              headerShown: false,
-            }}
-          >
-            <Tab.Screen
-              name="HomeTab"
-              component={HomeTab}
-              options={{
-                headerShown: false,
-                headerTitle: false,
-                tabBarIcon: ({ focused }) => (
-                  <TabIcon  name="home" focused={focused} />
-                ),
-                title: ''
-              }}
-            />
-            <Tab.Screen
-              name="PublishTab"
-              component={PublishTab}
-              options={{
-                headerShown: false,
-                tabBarIcon: ({ focused }) => (
-                  <TabIcon name="add-circle" focused={focused} />
-                ),
-                title: ``
-              }}
-            />
-            <Tab.Screen
-              name="WikiTab"
-              component={WikiTab}
-              options={{
-                
-                tabBarIcon: ({ focused }) => (
-                  <TabIcon name="planet" focused={focused} />
-                ),
-                title: ''
-              }}
-            />
-          </Tab.Navigator>
-        </NavigationContainer>
-        </AuthProvider>
+      <Main />
+    </AuthProvider>
   );
+}
+
+function Main() {
+  const { session } = useUserInfo();
+  const [isSessionActive, setIsSessionActive] = useState(session);
+
+  useEffect(() => {
+    setIsSessionActive(session);
+  }, [session]);
+
+  return isSessionActive ? <Navigation /> : <UserAuth />;
 }
 
 const styles = StyleSheet.create({
---
+++ wickyandmorty/utils/Navigation.js
@@ -0,0 +1,70 @@
+import "react-native-url-polyfill/auto";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import TabIcon from "../components/Basic/TabIcons";
+import HomeTab from "../tabs/HomeTab";
+import WikiTab from "../tabs/WikiTab";
+import PublishTab from "../tabs/PublishTab";
+
+const Tab = createBottomTabNavigator();
+
+
+export default function App() {
+  return (
+        <NavigationContainer>
+          <Tab.Navigator 
+            screenOptions={{
+              tabBarStyle: styles.tabBar,
+              tabBarLabelStyle: { fontSize: 14, },
+              tabBarInactiveTintColor: "white",
+              tabBarActiveTintColor: "#97CE4C",
+              headerShown: false,
+            }}
+          >
+            <Tab.Screen
+              name="HomeTab"
+              component={HomeTab }
+              options={{
+                headerShown: false,
+                headerTitle: false,
+                tabBarIcon: ({ focused }) => (
+                  <TabIcon  name="home" focused={focused} />
+                ),
+                title: ''
+              }}
+            />
+            <Tab.Screen
+              name="PublishTab"
+              component={PublishTab}
+              options={{
+                headerShown: false,
+                tabBarIcon: ({ focused }) => (
+                  <TabIcon name="add-circle" focused={focused} />
+                ),
+                title: ``
+              }}
+            />
+            <Tab.Screen
+              name="WikiTab"
+              component={WikiTab}
+              options={{
+                
+                tabBarIcon: ({ focused }) => (
+                  <TabIcon name="planet" focused={focused} />
+                ),
+                title: ''
+              }}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  tabBar: {
+    flex: 1,
+    backgroundColor: "black",
+    maxHeight: 110,
+  },
+});
